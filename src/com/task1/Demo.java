3494d1999777272a41696d4256c98c24f5b776f0
Lavish <lavishjain36@gmail.com>
2024-12-23 19:17:03 +0530

    first commit

---
+++ src/com/task1/Demo.java
@@ -0,0 +1,11 @@
+package com.task1;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		int num1=10;
+		int num2=20;
+		System.out.println(num1+num2);
+	}
+
+}
---
+++ src/com/task1/Test.java
@@ -0,0 +1,10 @@
+package com.task1;
+
+//java class->Test
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Buddy.I hope you are doing well today.");
+	}
+
+}
